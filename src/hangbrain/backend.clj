dcc1144118dfba3f00a37705d48e0ec528efcddb
Rebecca Kelly <bk@ancilla.ca>
2021-09-28 23:09:05 -0400

    WIP support for message fetching and RECAP
    currently the returned messages have the right content but the to/from
    are kind of messed up

--- src/hangbrain/backend.clj
+++ src/hangbrain/backend.clj
@@ -58,16 +58,62 @@
                         [frame (wd/query-all ctx "div[spellcheck]")])) $)
         (some (fn [[frame divs]] (when (not-empty divs) [frame (first divs)])) $)))
 
-(defn send-message
-  [ctx msg]
-  (log/trace "sending message" msg)
-  (wd/wait-predicate
-    (partial find-input-div ctx)
-    {:timeout 5 :message "Couldn't find input div after 5 seconds"})
-  (let [[frame div] (find-input-div ctx)]
-    (log/trace "sending to" frame div)
-    (with-frame-el ctx frame
-      (wd/fill-el ctx div msg keys/enter))))
+(def js-read-messages
+  "Collect messages from the currently focused channel. Messages are returned in the order they appear on screen, and look like
+  {:author {:realname :user :host} :timestamp :html}
+  "
+  "
+  let map = (f,xs) => Array.prototype.map.call(xs, f);
+  let toUser = span => {
+    let name = span.dataset.name;
+    let [user,host] = span.dataset.hovercardId.split('@');
+    return {
+      realname: name,
+      user: user,
+      host: host,
+    };
+  };
+  let toMessage = elem => {
+    let html =
+      elem.querySelector('div[jsaction^=mouseenter] div[jsaction]').innerHTML
+      || elem.querySelector('div[jsaction^=mouseenter] a').href
+      || 'ERROR: unable to process message';
+    if (html == '') {
+      html = elem.querySelector()
+    }
+    return {
+     author: toUser(elem.querySelector('span[data-member-id]')),
+     timestamp: elem.querySelector('span[data-absolute-timestamp]').dataset.absoluteTimestamp,
+     html: html
+    };
+  };
+  return map(toMessage, document.querySelectorAll('c-wiz[data-is-user-topic=true]'));
+  ")
+
+(defn process-image
+  [img]
+  (let [src (second (re-find #"src=\"([^\"]+)\"" img))
+        alt (second (re-find #"alt=\"([^\"]+)\"" img))]
+    (if alt alt (str "[img " src "]"))))
+
+(defn process-link
+  [a]
+  (let [href (second (re-find #"href=\"([^\"]+)\"" a))
+        text (string/replace a #"</?a[^>]*>" "")]
+    (if (or (= text href)
+          (= text (string/replace href #"^https?://" "")))
+      text
+      (str text " [" href "]"))))
+
+(def formatting
+  [[#"^<i>(.*)</i>$" "\u0001ACTION$1\u0001"]
+   [#"</?b>" "\u0002"]
+   [#"</?i>" "\u001D"]
+   [#"</?u>" "\u001F"]
+   [#"&nbsp;" " "]
+   [#"<img [^>]+>" process-image]
+   [#"<a [^>]+>.*?</a>" process-link]
+   [#"</?span[^>]*>" ""]])
 
 (defn ->IRCNick
   [name]
@@ -83,6 +129,43 @@
       (string/replace $ #"[ ,]+", "-")
       (str "#" $)))
 
+(defn- ->IRCMessage
+  [message]
+  (assoc message
+    :author (assoc (:author message) :name (->IRCNick (get-in message [:author :realname])))
+    :text (reduce
+            ; (fn [text [pattern replacement]] (string/replace text pattern replacement))
+            (partial apply string/replace)
+            (:html message)
+            formatting)))
+
+(defn- read-messages
+  [ctx]
+  ; TODO factor this out, maybe make find-input-div itself block
+  (wd/wait-predicate
+    (partial find-input-div ctx)
+    {:timeout 5 :message "Couldn't find input div after 5 seconds"})
+  (let [iframe (first (find-input-div ctx))]
+    (with-frame-el ctx iframe
+      (->> (wd/js-execute ctx js-read-messages)
+           (map ->IRCMessage)))))
+
+(defn- read-messages-since
+  [ctx timestamp]
+  (->> (read-messages ctx)
+       (drop-while #(<= timestamp (:timestamp %)))))
+
+(defn send-message
+  [ctx msg]
+  (log/trace "sending message" msg)
+  (wd/wait-predicate
+    (partial find-input-div ctx)
+    {:timeout 5 :message "Couldn't find input div after 5 seconds"})
+  (let [[frame div] (find-input-div ctx)]
+    (log/trace "sending to" frame div)
+    (with-frame-el ctx frame
+      (wd/fill-el ctx div msg keys/enter))))
+
 (def js-list-room-channels
   "
   let map = (f,xs) => Array.prototype.map.call(xs, f);
@@ -91,6 +174,7 @@
       id: span.dataset.groupId,
       timestamp: span.querySelector('span[data-absolute-timestamp]')?.dataset.absoluteTimestamp || 0,
       topic: span.querySelector('span[role=presentation][title]').title,
+      unread: span.innerText.includes('Unread'),
       users: [],
       count: 0,
       type: 'channel'
@@ -121,11 +205,12 @@
     let id = span.dataset.groupId;
     let users = map(toUser, span.querySelectorAll('span[data-member-id]'));
     let timestamp = span.querySelector('span[data-absolute-timestamp]')?.dataset.absoluteTimestamp || 0;
+    let unread = span.innerText.includes('Unread');
     if (users.length > 1) {
       let topic = span.querySelector('span[role=presentation][title]').title;
-      return { id: id, users: users, topic: topic, timestamp: timestamp, count: users.length, type: 'channel' };
+      return { id: id, users: users, topic: topic, timestamp: timestamp, unread: unread, count: users.length, type: 'channel' };
     } else if (users.length == 1) {
-      return Object.assign(users[0], { id: id, timestamp: timestamp, type: 'dm' });
+      return Object.assign(users[0], { id: id, timestamp: timestamp, unread: unread, type: 'dm' });
     } else {
       return false;
     }
@@ -135,11 +220,14 @@
 
 (defn ->ChatInfo [info]
   (log/trace "->ChatInfo" info)
-  (let [info (update info :type keyword)]
-    (case (:type info)
-      :dm (assoc info :name (->IRCNick (:realname info)))
-      :channel (assoc info :name (->IRCChannel (:topic info)))
-    )))
+  (let [info (update info :type keyword)
+        seen (if (:unread info) 0 (:timestamp info))
+        name (case (:type info)
+               :dm (->IRCNick (:realname info))
+               :channel (->IRCChannel (:topic info)))]
+    (-> info
+        (assoc :seen seen :name name)
+        (dissoc :unread))))
 
 (defn list-dms [ctx iframe]
   (log/trace "Getting chats in iframe" iframe)
@@ -147,7 +235,6 @@
     (->> (wd/js-execute ctx js-list-user-channels)
          (map ->ChatInfo)
          )))
-         ; (into {})))) ; for later, right now it expects a seq
 
 (defn list-rooms [ctx iframe]
   (log/trace "Getting rooms in iframe" iframe)
@@ -171,33 +258,52 @@
     (wd/go "https://chat.google.com/")
     (wd/wait-exists "div#talk_roster" {:timeout 30 :interval 1})))
 
-(defn- timestamp [] (quot (System/currentTimeMillis) 1000))
+(defn update-or-insert
+  "Update a cache entry or insert a new one.
+  The :seen field from the old entry, if present, is retained, so we don't forget about read status every time we refresh the cache."
+  [chat chat']
+  (merge chat' (select-keys chat [:seen])))
 
-(defn- rebuild-cache [ctx]
+(defn rebuild-cache
+  "Rebuild the channel cache.
+  This rescans the chat list completely and populates the cache from it, retaining the :seen field from any previous cache entries."
+  ; TODO: we should clear cache entries that no longer exist on the server!
+  ; this is probably easier if list-all returns a map rather than a list, and then we can do cache -> (select-keys cache (keys new-channels))
+  [cache ctx]
   (reduce
     (fn [cache chat]
       (log/trace "Cache update:" (:name chat) "->" (:id chat))
-      (assoc cache (:name chat) chat))
-    {:ts (timestamp)}
+      (update cache (:name chat) update-or-insert chat))
+    cache
     (list-all ctx)))
 
-(defn rebuild-if-old
-  [cache ctx]
-  (if (> (- (timestamp) (:ts @cache)) (* 60 60))
-    (do
-      (log/info "Cache ts" (:ts @cache) "older than now of" (timestamp) "- rebuilding")
-      (reset! cache (rebuild-cache ctx)))
-    @cache))
-
 (defn- read-cache
-  ([cache ctx]
-   (vals (rebuild-if-old cache ctx)))
-  ([cache ctx key]
+  ([cache ctx] ; read all cache entries -> always rebuild
+   (vals (swap! cache rebuild-cache ctx)))
+  ([cache ctx key] ; read specific entry -> rebuild on cache miss
    (if (contains? @cache key)
      (@cache key)
      (do
        (log/warn "Cache miss on key" key "- rebuilding")
-       (get (reset! cache (rebuild-cache ctx)) key)))))
+       (get (swap! cache rebuild-cache ctx) key)))))
+
+(defn clear-unread
+  [cache channel]
+  (swap! cache
+         (fn [cache]
+           (assoc-in cache [channel :seen]
+             (get-in cache [channel :timestamp])))))
+
+(defn- post-process
+  [chat messages]
+  (if (= :dm (:type chat))
+    (map
+      (fn [message]
+        ))
+    (map
+      (fn [message]
+        (assoc message))))
+  )
 
 (defn create :- (s/protocol ZeiatBackend)
   [opts]
@@ -207,17 +313,19 @@
         ; - whenever a cache miss occurs
         ; - whenever a request for the entire cache (listUsers/Channels) occurs
         ;   and the cache contents are more than an hour old
-        cache (atom {:ts 0})]
+        cache (atom {})]
     (reify ZeiatBackend
       (connect [this]
         (log/info "Connecting to Google Chat...")
-        (swap! ctx startup-browser (:browser opts) (:profile opts)))
+        (swap! ctx startup-browser (:browser opts) (:profile opts))
+        (log/info "Connected."))
       (disconnect [this]
         (if @ctx
           (do
             (log/info "Shutting down Google Chat connection...")
             (wd/quit @ctx)
-            (reset! ctx nil))
+            (reset! ctx nil)
+            (log/info "Shut down."))
           (log/info "Ignoring shutdown request, backend already shut down.")))
       (listChannels [this]
         (filter #(= :channel (:type %))
@@ -226,14 +334,33 @@
         (filter #(= :dm (:type %))
           (read-cache cache @ctx)))
       (listUnread [this]
-        (log/trace "stub: list-unread" this)
-        [])
+        (filter #(not= (:seen %1) (:timestamp %1))
+          (read-cache cache @ctx)))
       (listMembers [this channel]
-        (log/trace "stub: list-members" this channel)
-        [])
+        ; TODO: this works only for channels which have a list of users in the cache entry,
+        ; which does not include named channels; to get the user list for those, it's kind of
+        ; grody. We need to select the channel, then click on the "channel name [n members]"
+        ; button in the top left, then "view members", then find the iframe that filled in and
+        ; read the list out of that...this is gross enough that it may be easier to just scan
+        ; the channel history, build a user list based on that, and if it changes re-issue a
+        ; NAMES command.
+        ; This does require some way to tell Zeiat that something changed...eurgh. We're going
+        ; to need to extend the API to allow the backend to send events back to Zeiat in some
+        ; capacity, I think.
+        (when-let [chat (read-cache cache @ctx channel)]
+          (:users chat)))
       (readMessages [this channel]
-        (log/trace "stub: read-messages" this channel)
-        [])
+        (log/trace "Reading messages from" channel)
+        (when-let [chat (read-cache cache @ctx channel)]
+          (select-chat @ctx (:id chat))
+          (clear-unread cache channel)
+          (post-process chat (read-messages @ctx))))
+      (readNewMessages [this channel]
+        (when-let [chat (read-cache cache @ctx channel)]
+          (select-chat @ctx (:id chat))
+          (let [seen (:seen chat)]
+            (clear-unread cache channel)
+            (post-process chat (read-messages-since @ctx seen)))))
       (writeMessage [this channel message]
         ; TODO handle translation from IRC formatting codes to Hangouts markdownish codes
         ; https://support.google.com/chat/answer/7649118?hl=en
